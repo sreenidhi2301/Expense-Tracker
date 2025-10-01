1cb2b9db2fbe7f51c9670503571c9b21fe57848e
sreenidhi2301 <kondansreenidhigamil.com>
2025-10-01 15:21:36 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AllinOne from './components/AllinOne'
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <AllinOne />
+    </div>
+  );
+}
+
+export default App;
